b45039bd79013b5eee2deb01ae5dc8fe4f6ba00d
reuven9632 <reuven9632@gmail.com>
2020-12-07 16:24:20 +0200

    test

---
+++ src/test/java/com/ITReuvenBlog/WebBlog/WebBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITReuvenBlog.WebBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
