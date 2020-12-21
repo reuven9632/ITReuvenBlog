30b738c069cdae51b7c635d1899db659babb6561
reuven9632 <reuven9632@gmail.com>
2020-12-21 21:20:01 +0200

    Add to database, <dependency> =>jpa, mysql-connector-java

--- src/main/java/com/ITReuvenBlog/WebBlog/controllers/BlogController.java
+++ src/main/java/com/ITReuvenBlog/WebBlog/controllers/BlogController.java
@@ -1,8 +1,13 @@
 package com.ITReuvenBlog.WebBlog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogController {
+    @GetMapping("/blog")
+    public String BlogMain(Model model){
+        return "blog";
+    }
 }
