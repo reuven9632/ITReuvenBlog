3fd1566c952b600266ab3f20b2c699010931591d
reuven9632 <reuven9632@gmail.com>
2020-12-21 23:33:42 +0200

    post table

--- src/main/java/com/ITReuvenBlog/WebBlog/controllers/BlogController.java
+++ src/main/java/com/ITReuvenBlog/WebBlog/controllers/BlogController.java
@@ -1,13 +1,22 @@
 package com.ITReuvenBlog.WebBlog.controllers;
 
+import models.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import repositoryes.PostRepository;
 
 @Controller
 public class BlogController {
+
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
     public String BlogMain(Model model){
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog";
     }
 }
