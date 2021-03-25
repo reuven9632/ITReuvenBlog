fd6ffe039c210d755cc35513a065bd15eff58972
reuven9632 <reuven9632@gmail.com>
2021-03-25 10:05:46 +0200

    migrate registration from controller to UserService

--- src/main/java/com/ITReuvenBlog/WebBlog/controllers/RegistrationController.java
+++ src/main/java/com/ITReuvenBlog/WebBlog/controllers/RegistrationController.java
@@ -3,6 +3,7 @@ package com.ITReuvenBlog.WebBlog.controllers;
 import com.ITReuvenBlog.WebBlog.models.Role;
 import com.ITReuvenBlog.WebBlog.models.User;
 import com.ITReuvenBlog.WebBlog.repositoryes.UserRepository;
+import com.ITReuvenBlog.WebBlog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,9 +15,11 @@ import java.util.Collections;
 
 @Controller
 public class RegistrationController {
-    @Autowired
-    private UserRepository userRepository;
+    /*@Autowired
+    private UserRepository userRepository;*/
 
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration(Model model){
@@ -25,15 +28,12 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, @RequestParam String username, @RequestParam String password, Model model){
-        User userFromDb = userRepository.findByUsername(user.getUsername());
 
-        if (userFromDb != null){
+        if (!userService.addUser(user)){
             model.addAttribute("message", "user exists");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
+
 
         /*   --------------------------*/
         /*User user = userRepository.findByUsername(username);
--- src/main/java/com/ITReuvenBlog/WebBlog/service/UserService.java
+++ src/main/java/com/ITReuvenBlog/WebBlog/service/UserService.java
@@ -1,11 +1,15 @@
 package com.ITReuvenBlog.WebBlog.service;
 
+import com.ITReuvenBlog.WebBlog.models.Role;
+import com.ITReuvenBlog.WebBlog.models.User;
 import com.ITReuvenBlog.WebBlog.repositoryes.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -15,5 +19,18 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
+    }
+
+    public boolean addUser(User user){
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null)
+            return false;
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+
+        return true;
     }
 }
